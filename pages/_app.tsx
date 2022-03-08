5a28edc8ab243070313590a41538046411f26c45
alexbrothers <alexander.b.brothers@gmail.com>
2022-03-07 21:37:08 -0500

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,31 @@ import '@fontsource/roboto/700.css';
 import { Container, CssBaseline } from '@mui/material';
 import HideAppBar from '../components/HideAppBar';
 import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   let theme = createTheme();
   theme = responsiveFontSizes(theme);
+  const googleAnalyticsTag = process.env.GOOGLE_ANALYTICS_TAG;
 
   return (
     <ThemeProvider theme={theme}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsTag}`}
+      />
+      <Script strategy="lazyOnload">
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${googleAnalyticsTag}');
+          `
+        }
+      </Script>
       <CssBaseline />
       <HideAppBar />
       <Container>
