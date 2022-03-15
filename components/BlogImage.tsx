51430406c8b1c588c70f0fdf12a5d0f95540738f
alexbrothers <alexander.b.brothers@gmail.com>
2022-03-15 00:44:25 -0400

    Use plain img tag

--- components/BlogImage.tsx
+++ components/BlogImage.tsx
@@ -1,5 +1,4 @@
 import { Box } from "@mui/material";
-import Image from 'next/image';
 import React from "react";
 
 interface BlogImageProps {
@@ -12,40 +11,20 @@ interface BlogImageProps {
 export default function BlogImage(props: BlogImageProps) {
     return (
         <>
-            <Box sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                paddingTop: "20px",
-                paddingBottom: "20px",
-                height: {
-                    xs: "30vh",
-                    md: "50vh"
-                },
-                width: "100%"
-            }}>
-                    <Box 
-                        component="div" 
-                        sx={{
-                            width: "100%",
-                            height: "auto",
-                            position: "relative",
-                            ":hover": {
-                                cursor: "pointer",
-                            }
-                        }}
-                        onClick={e => window.open(`${props.src}`, "_blank")}
-                    >
-                        <Image
-                            src={props.src}
-                            alt={props.alt}
-                            layout="responsive"
-                            objectFit="contain"
-                            width={props.width}
-                            height={props.height}
-                            quality={100}
-                        />
-                    </Box>
+            <Box 
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    paddingTop: "20px",
+                    paddingBottom: "20px",
+                    ":hover": {
+                        cursor: "pointer",
+                    }
+                }}
+                onClick={e => window.open(`${props.src}`, "_blank")}
+            >
+                    <img src={props.src} width={"100%"} />
             </Box>
         </>
     )
