24892ef626059d240c7315113e689e019af8163f
alexbrothers <alexander.b.brothers@gmail.com>
2022-03-20 13:33:05 -0400

    Add / in front of home in sitemap

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -7,7 +7,7 @@ function generateSiteMap(blogIds: string[]) {
     return `<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
-            <loc>${baseUrl}</loc>
+            <loc>${baseUrl}/</loc>
         </url>
         <url>
             <loc>${baseUrl}/about</loc>
